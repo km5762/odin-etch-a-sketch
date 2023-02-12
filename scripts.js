f4552d64b707a99d0ca4138976c43a632655c53f
km5762 <km5762@DESKTOP-587P2TU>
2023-02-12 14:21:56 -0500

    Add event listeners to properly color grid

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 function initializeGrid(size) {
     const gridContainer = document.querySelector('.gridContainer');
+    let mouseDown = false;
+    document.addEventListener('mousedown', () => {mouseDown = true;});
+    document.addEventListener('mouseup', () => {mouseDown = false;});
     for (let i = 0; i < size; i++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
@@ -7,7 +10,7 @@ function initializeGrid(size) {
         for (let j = 0; j < size; j++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
-            gridSquare.addEventListener('hover', () => {gridSquare.setAttribute('style', 'background-color:black')})
+            gridSquare.addEventListener('mouseover', () => {if (mouseDown == true) gridSquare.setAttribute('style', 'background-color:black')});
             gridRow.appendChild(gridSquare);
         }
         gridContainer.appendChild(gridRow);
