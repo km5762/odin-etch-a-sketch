1d63d26cebf3bb730dc6ac3418a386a4dd830361
km5762 <km5762@DESKTOP-587P2TU>
2023-02-15 16:15:31 -0500

    Add reset button and linked color picker functionality to drawing color

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+let color = 'black';
+const colorPicker = document.getElementById("colorPicker");
+const resetButton = document.getElementById("resetButton");
+
+colorPicker.addEventListener('input', (e) => {
+  color = e.target.value;
+});
+
+resetButton.addEventListener('click', () => {
+    const gridSquares = document.querySelectorAll('.gridSquare');
+
+    gridSquares.forEach((square) => {
+        square.setAttribute('style', 'background-color:white')
+    })
+})
+
 function initializeGrid(size) {
     const gridContainer = document.querySelector('.gridContainer');
     let mouseDown = false;
@@ -10,15 +26,13 @@ function initializeGrid(size) {
         for (let j = 0; j < size; j++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
-            gridSquare.addEventListener('mouseover', () => {if (mouseDown == true) gridSquare.setAttribute('style', 'background-color:black')});
-            gridSquare.addEventListener('click', () => {gridSquare.setAttribute('style', 'background-color:black')});
+            gridSquare.addEventListener('mouseover', () => {if (mouseDown == true) gridSquare.setAttribute('style', `background-color:${color}`)});
+            gridSquare.addEventListener('click', () => {gridSquare.setAttribute('style', `background-color:${color}`)});
             gridRow.appendChild(gridSquare);
         }
         gridContainer.appendChild(gridRow);
     }
 }
-
-
 
 
 
